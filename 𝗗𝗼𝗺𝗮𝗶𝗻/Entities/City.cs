b2108fec7c45e08d83b67b88e780b029fe2f676e
yousef <yousefwalid950@gmail.com>
2025-12-26 01:26:12 +0200

    Add project files.

---
+++ 𝗗𝗼𝗺𝗮𝗶𝗻/Entities/City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class City
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Guid CountryId { get; private set; }
+
+        private City() { }
+
+        public City(string name, Guid countryId)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            CountryId = countryId;
+        }
+    }
+
+}
