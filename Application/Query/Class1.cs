2c5e475ab24b12a50977053472a7cd41e762c1e8
Youssef Waleed <youssef.waleed22470@ci.menofia.edu.eg>
2026-01-05 04:40:55 +0200

    organizing

---
+++ Application/Query/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fas7ny.Application.Query
+{
+    internal class Class1
+    {
+    }
+}
